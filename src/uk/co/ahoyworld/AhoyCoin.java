951d1c3a7bd16bb4d7b657ccf284831340d07407
Jack Williams <jpwilliams@ahoyworld.co.uk>
2012-05-25 01:49:07 +0100

    Beta 0.2r13

--- src/uk/co/ahoyworld/AhoyCoin.java
+++ src/uk/co/ahoyworld/AhoyCoin.java
@@ -6,8 +6,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.List;
- 
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -16,23 +16,38 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
  
 public class AhoyCoin extends JavaPlugin {
- 
+	
+	//create the variables for our config files
     File configFile;
     File townsFile;
     File basePriceFile;
     FileConfiguration config;
     FileConfiguration towns;
     FileConfiguration basePrices;
+    
+    //create a logger called "log" so we can output to the console
     Logger log;
     
+    //set up an "[AhoyCoin]" prefix that'll go at the beginning of all our messages
+    String pre = ChatColor.GOLD + "[AhoyCoin]" + ChatColor.WHITE + " ";
+    
     public void onEnable()
-    {        
+    {
+    	// get "replenishtimer" from items in towns.yml
+    	// if > 0, start a thread running with the value as a start offset
+    	// 		Say an item replenishes every 200 ticks. After 50 ticks, the server restarts and 50 is saved as replenishtimer
+    	//		onEnable, set the initial delay (initial, regular repeat time) to (replenishtime - replenishtimer (i.e. 200 - 50)).
+    	//		Using this technique, the replenishment will keep the position of longer replenishment times
+    	
+    	//register our event that triggers when a play clicks a block
     	new Event_onBlockClick(this);
     	
+    	//ensure the config files are created
         configFile = new File(getDataFolder(), "config.yml");
         townsFile = new File(getDataFolder(), "towns.yml");
         basePriceFile = new File(getDataFolder(), "basePrice.yml");
      
+        //error when first running? show us!
         try
         {
             firstRun();
@@ -40,28 +55,35 @@ public class AhoyCoin extends JavaPlugin {
             e.printStackTrace();
         }
      
+        //define our config files as "YamlConfigurations"
         config = new YamlConfiguration();
         towns = new YamlConfiguration();
         basePrices = new YamlConfiguration();
-     
+
+        //run the function "loadYamls"
         loadYamls();
-     
+        
         log = this.getLogger();
+        //tell us that the plugin has now been successfully enabled
         log.info("Plugin enabled.");
     }
  
+    //function that saves the config files
     public void saveYamls()
     {
         try
         {
+        	//save our config files
             config.save(configFile);
             towns.save(townsFile);
             basePrices.save(basePriceFile);
+        //problem? tell us!
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
  
+    //function that loads the config files
     public void loadYamls()
     {
         try
@@ -113,6 +135,10 @@ public class AhoyCoin extends JavaPlugin {
  
     public void onDisable()
     {
+    	// interrupt all running async threads and get their current time (split) in ticks
+    	// save this value in towns.yml as "replenishtimer" under the appropriate item
+    	// refer to onEnable for the rest...
+    	
         saveYamls();
         log.info("Plugin disabled.");
     }
@@ -123,8 +149,12 @@ public class AhoyCoin extends JavaPlugin {
         this.saveConfig();
     }
     
+    //this is a boolean (true/false) that manages when a player sends a /command
+    //if it returns true, the command is treated as recognised and so doesn't do anything special (apart from what we tell it to do)
+    //if it returns false, the help text is displayed. We specify this in the plugin.yml file under the "commands" node
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
     {
+    	//this just creates a variable named "player" that will be the player that sent the command
     	Player player = (Player) sender;
         if (cmd.getName().equalsIgnoreCase("ac"))
         {
@@ -132,8 +162,14 @@ public class AhoyCoin extends JavaPlugin {
             {
                 if (args[0].equalsIgnoreCase("town"))
                 {
-                    player.sendMessage("[AhoyCoin] Usage: /AC town [list, add, remove]");
+                    player.sendMessage(pre + "Usage: /AC town [list, add, remove]");
                     return true;
+                }
+                
+                if (args[0].equalsIgnoreCase("reload"))
+                {
+                	this.reloadConfig();
+                	return true;
                 }
             }
             if (args.length == 2)
@@ -142,33 +178,30 @@ public class AhoyCoin extends JavaPlugin {
                 {
                     if (args[1].equalsIgnoreCase("list"))
                     {
-                		List<String> townslist = towns.getStringList("towns");
-                		String townsListStr = "";
-                		
-                	    if (!townslist.isEmpty())
-                	    {
-                	    	for (int i = 0; i < townslist.size(); i++)
-                	    	{
-                	    		townsListStr = townsListStr + townslist.get(i) + ", ";
-                	    	}
-                	    	player.sendMessage("[AhoyCoin] List of created towns:");
-                	    	player.sendMessage("[AhoyCoin] " + townsListStr);
-                	    } else {
-                	    	player.sendMessage("[AhoyCoin] No towns have been created.");
-                	    }
-                	    	
-                        return true;
-                		
+                    	if (!towns.getKeys(false).isEmpty())
+                    	{
+                    		String townsListStr = "";
+                        	for (String str : towns.getKeys(false))
+                        	{
+                        		townsListStr = townsListStr + str + ", ";
+                        	}
+                        	player.sendMessage(pre + "List of created towns:");
+                        	player.sendMessage(pre + "" + townsListStr);
+                    	} else {
+                    		player.sendMessage(pre + "No towns have been created.");
+                    	}
+                    	
+                        return true;	
                     }
                     if (args[1].equalsIgnoreCase("add"))
                     {
-                        player.sendMessage("[AhoyCoin] Type the name of the town you would like to add");
-                        player.sendMessage("[AhoyCoin] Usage: /AC town add [name]");
+                        player.sendMessage(pre + "Type the name of the town you would like to add");
+                        player.sendMessage(pre + "Usage: /AC town add [name]");
                         return true;
                     }
                     if (args[1].equalsIgnoreCase("remove"))
                     {
-                    	player.sendMessage("[AhoyCoin] Name of town to remove not provided.");
+                    	player.sendMessage(pre + "Name of town to remove not provided.");
                     	return true;
                     }
                 }
@@ -180,52 +213,165 @@ public class AhoyCoin extends JavaPlugin {
             	{
             		if (args[1].equalsIgnoreCase("add"))
             		{
-            			List<String> townslist = towns.getStringList("towns");
-            			townslist.add(args[2].toString());
-            			towns.set("towns", townslist);
+            			int defaultTax = config.getInt("defaults.tax");
+            			
+            			towns.set(args[2].toString() + ".tax", defaultTax);
             			saveYamls();
-            			player.sendMessage("[AhoyCoin] Town \"" + args[2].toString() + "\" added.");
+            			player.sendMessage(pre + "Town \"" + args[2].toString() + "\" added.");
             			
             			return true;
             		}
             		
                 	if (args[1].equalsIgnoreCase("remove"))
                 	{
-                		List<String> townslist = towns.getStringList("towns");
-                		if (!townslist.isEmpty())
-                	    {
-                			String townToRemove = args[2];
-                	    	for (int i = 0; i < townslist.size(); i++)
-                	    	{
-                	    		if (townToRemove.equalsIgnoreCase(townslist.get(i)))
-                	    		{
-                	    			String townRemoved = townslist.get(i).toString();
-                	    			townslist.remove(i);
-                	    			towns.set("towns", townslist);
-                	    			saveYamls();
-                	    			player.sendMessage("[AhoyCoin] Town \"" + townRemoved + "\" removed.");
-                	    			return true;
-                	    		}
-                	    	}
-	        	    		player.sendMessage("[AhoyCoin] Town \"" + townToRemove + "\" does not exist.");
+                		if (!towns.getKeys(false).isEmpty())
+                		{
+                			for (String str : towns.getKeys(false))
+                			{
+                				if (str.equalsIgnoreCase(args[2].toString()))
+                				{
+                					towns.set(str, null);
+                					saveYamls();
+                        			player.sendMessage(pre + "Town \"" + args[2].toString() + "\" removed.");
+                        			return true;
+                				}
+                			}
+	        	    		player.sendMessage(pre + "Town \"" + args[2].toString() + "\" does not exist.");
 	        	    		return true;
-                	    } else {
-                	    	player.sendMessage("[AhoyCoin] There are no towns to remove.");
+                		} else {
+                	    	player.sendMessage(pre + "There are no towns to remove.");
                 	    	return true;
-                	    }
+                		}
                 	}
+            	}
+            	
+            	if (args[0].equalsIgnoreCase("pricereset"))
+            	{
+            		String town = args[1].toString();
+            		String item = args[2].toString().toLowerCase();
+            		
+            		if (towns.getKeys(true).contains(town + ".items." + item))
+            		{
+            			// item exists
+            			towns.set(town + ".items." + item, null);
+            			saveYamls();
+            			player.sendMessage(pre + "Item \"" + item + "\" for town \"" + town + "\" reset to defaults.");
+            			return true;
+            		} else {
+            			// item doesn't exist
+            			player.sendMessage(pre + "Item \"" + item + "\" for town \"" + town + "\" already set to defaults.");
+            			return true;
+            		}
+            	}
+            	
+            	if (args[0].equalsIgnoreCase("taxset"))
+            	{
+            		boolean townExists = false;
+            		String town = "";
+            		for (String str : towns.getKeys(false))
+            		{
+            			if (str.equalsIgnoreCase(args[1]))
+            			{
+            				townExists = true;
+            				town = str;
+            				break;
+            			}
+            		}
+            		if (townExists)
+            		{
+            			Integer tax = Integer.parseInt(args[2]);
+            			towns.set(town + ".tax", tax);
+            			saveYamls();
+            			player.sendMessage(pre + "Tax rate for town \"" + town + "\" set to " + tax.toString() + "%.");
+            			return true;
+            		} else {
+            			player.sendMessage(pre + "Town \"" + town + "\" does not exist.");
+            			return true;
+            		}
             	}
             }
             
-            if (args.length > 3)
+            if (args.length >= 4)
             {
-            	if (args[0].equalsIgnoreCase("town"))
+            	if (args[0].equalsIgnoreCase("priceset"))
             	{
-            		if (args[1].equalsIgnoreCase("add") || args[1].equalsIgnoreCase("remove"))
+            		String town = "";
+            		String item = "";
+            		
+            		//Check town exists
+            		boolean townExists = false;
+            		for (String str : towns.getKeys(false))
             		{
-            			player.sendMessage("[AhoyCoin] Invalid arguments!");
-            			player.sendMessage("[AhoyCoin] Town names can not contain spaces!");
-            			
+            			if (args[1].toString().equalsIgnoreCase(str))
+            			{
+            				townExists = true;
+            				town = str;
+            				break;
+            			}
+            		}
+            		if (townExists)
+            		{
+            			//Check item exists
+            			boolean itemExists = false;
+            			for (String str : basePrices.getKeys(false))
+            			{
+            				if (args[2].toString().equalsIgnoreCase(str))
+            				{
+            					itemExists = true;
+            					item = str;
+            					break;
+            				}
+            			}
+            			if (itemExists)
+            			{
+            				//Submit!
+            				player.sendMessage(pre + "Setting custom item for " + town + ":");
+            				String reportStr = "Item \"" + item + "\"";
+            				if (args.length > 3)
+            				{
+            					//Price
+            					towns.set(town + ".items." + item + ".price", Integer.parseInt(args[3]));
+            					reportStr = reportStr + " now sells for " + args[3].toString();
+            				}
+            				if (args.length > 4)
+            				{
+            					//Max Stock
+            					towns.set(town + ".items." + item + ".maxstock", Integer.parseInt(args[4]));
+            					reportStr = reportStr + " with a maximum stock of " + args[4].toString();
+            				} else if (towns.getConfigurationSection(town + ".items." + item).getKeys(false).contains("maxstock")) {
+            					reportStr = reportStr + " with a maximum stock of " + towns.getString(town + ".items." + item + ".maxstock");
+            				} else {
+            					reportStr = reportStr + " with a maximum stock of " + basePrices.getString(item + ".maxstock");
+            				}
+            				if (args.length > 5)
+            				{
+            					//Replenish every X days
+            					towns.set(town + ".items." + item + ".replenishtime", Integer.parseInt(args[5]));
+            					reportStr = reportStr + ", replenishing every " + args[5].toString() + " days";
+            				} else if (towns.getConfigurationSection(town + ".items." + item).getKeys(false).contains("replenishtime")) {
+            					reportStr = reportStr + ", replenishing every " + towns.getString(town + ".items." + item + ".replenishtime");
+            				} else {
+            					reportStr = reportStr + ", replenishing every " + basePrices.getString(item + ".replenishtime") + " days";
+            				}
+            				if (args.length > 6)
+            				{
+            					//Replenish Amount
+            					towns.set(town + ".items." + item + ".replenishamount", Integer.parseInt(args[6]));
+            					reportStr = reportStr + " for " + args[6].toString() + ".";
+            				} else if (towns.getConfigurationSection(town + ".items." + item).getKeys(false).contains("replenishamount")) {
+            					reportStr = reportStr + " for " + towns.getString(town + ".items." + item + ".replenishamount");
+            				} else {
+            					reportStr = reportStr + " for " + basePrices.getString(item + ".replenishamount") + ".";
+            				}
+            				saveYamls();
+            				player.sendMessage(reportStr);
+            				return true;
+            			} else {
+            				player.sendMessage(pre + "Item \"" + args[2].toString() + "\" does not exist.");
+            				return true;
+            			}
+            		} else {
+            			player.sendMessage(pre + "Town \"" + args[1].toString() + "\" does not exist.");
             			return true;
             		}
             	}
@@ -234,4 +380,3 @@ public class AhoyCoin extends JavaPlugin {
         return false;
     }
 }
- 
--- src/uk/co/ahoyworld/Event_onBlockClick.java
+++ src/uk/co/ahoyworld/Event_onBlockClick.java
@@ -1,34 +1,198 @@
 package uk.co.ahoyworld;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class Event_onBlockClick implements Listener 
 {	
+	private AhoyCoin plugin;
+	
 	public Event_onBlockClick(AhoyCoin plugin)
 	{
+		this.plugin = plugin;
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
 	}
 	
 	@EventHandler
 	public void blockLeftClick (PlayerInteractEvent event)
-	{
+	{		
 		if (event.getAction() == Action.LEFT_CLICK_BLOCK)
 		{
-			if (event.getClickedBlock().getType() == Material.SIGN_POST || event.getClickedBlock().getType() == Material.WALL_SIGN)
+			if (event.getClickedBlock().getState() instanceof Sign)
 			{
-				Player player = event.getPlayer();
-				player.sendMessage("Left clicked on a sign.");
+				Sign sign = (Sign) event.getClickedBlock().getState();
+				if (sign.getLine(0).equalsIgnoreCase("[Vendor]"))
+				{
+					Player player = event.getPlayer();
+					// player.sendMessage("Creating sign...");
+					final String [] signText = sign.getLines();
+					if (!(signText.length < 4))
+					{
+						String townName = signText[1];
+						String itemName = signText[2].toLowerCase();
+						Integer quantity = Integer.parseInt(signText[3]);
+						if (plugin.towns.getKeys(false).contains(townName))
+						{
+							// player.sendMessage("Town \"" + townName + "\" exists.");
+							if (plugin.basePrices.getKeys(false).contains(itemName))
+							{
+								// player.sendMessage("Item \"" + itemName + "\" exists.");
+								if (quantity <= 64 && quantity <= plugin.basePrices.getInt(itemName + ".maxstock"))
+								{
+									// player.sendMessage("Quantity \"" + quantity.toString() + "\" valid.");
+									
+									// Create the sign.
+									sign.setLine(0, (ChatColor.BLUE + sign.getLine(0)));
+									sign.update();
+									// Ugly, UGLY code.
+									// Change this to get the specifically-set maxstock settings (in towns.yml) if available
+									// Also, use "if (!plugin.towns.getKeys(true).contains(townName + ".items." + itemName + ".curstock"))
+									if (plugin.towns.getConfigurationSection(townName).getKeys(false).contains("items"))
+									{
+										if (plugin.towns.getConfigurationSection(townName + ".items").getKeys(false).contains(itemName))
+										{
+											if (!plugin.towns.getConfigurationSection(townName + ".items." + itemName).getKeys(false).contains("curstock"))
+											{
+												Integer maxstock = plugin.basePrices.getInt(itemName + ".maxstock");
+												plugin.towns.set(townName + ".items." + itemName + ".curstock", maxstock);
+												plugin.saveYamls();
+												player.sendMessage(plugin.pre + "Sign created!");
+											} else {
+												player.sendMessage(plugin.pre + "Apparently current stock is already assigned.");
+											}
+										} else {
+											Integer maxstock = plugin.basePrices.getInt(itemName + ".maxstock");
+											plugin.towns.set(townName + ".items." + itemName + ".curstock", maxstock);
+											plugin.saveYamls();
+											player.sendMessage(plugin.pre + "Sign created!");
+										}
+									} else {
+										Integer maxstock = plugin.basePrices.getInt(itemName + ".maxstock");
+										plugin.towns.set(townName + ".items." + itemName + ".curstock", maxstock);
+										plugin.saveYamls();
+										player.sendMessage(plugin.pre + "Sign created!");
+									}
+									Integer replenishTime = -1;
+									if (plugin.towns.getKeys(true).contains(townName + ".items." + itemName + ".replenishTime"))
+									{
+										replenishTime = (plugin.towns.getInt(townName + ".items." + itemName + ".replenishtime") * 20);
+									} else {
+										replenishTime = (plugin.basePrices.getInt(itemName + ".replenishtime") * 20);
+									}
+									
+									plugin.towns.set(townName + ".items." + itemName + ".replenishtimer", 0);
+									plugin.getServer().getScheduler().scheduleAsyncRepeatingTask(plugin, new Runnable()
+									{
+										String townName = signText[1];
+										String itemName = signText[2];
+										 
+										public void run()
+										{
+											Integer replenishAmount = -1;
+											if (plugin.towns.getKeys(true).contains(townName + ".items." + itemName + ".replenishamount"))
+											{
+												replenishAmount = plugin.towns.getInt(townName + ".items." + itemName + ".replenishamount");
+											} else {
+												replenishAmount = plugin.basePrices.getInt(itemName + ".replenishamount");
+											}
+											
+											Integer maxStock = -1;
+											if (plugin.towns.getKeys(true).contains(townName + ".items." + itemName + ".maxstock"))
+											{
+												maxStock = plugin.towns.getInt(townName + ".items." + itemName + ".maxstock");
+											} else {
+												maxStock = plugin.basePrices.getInt(itemName + ".maxstock");
+											}											
+											
+											Integer oldCurStock = plugin.towns.getInt(townName + ".items." + itemName + ".curstock");
+											Integer newCurStock = -1;
+											if ((oldCurStock + replenishAmount) > maxStock)
+											{
+												// will go over max stock
+												newCurStock = maxStock;
+											} else {
+												// will not go over max stock
+												newCurStock = oldCurStock + replenishAmount;
+											}
+											
+											plugin.towns.set(townName + ".items." + itemName + ".curstock", newCurStock);
+											System.out.println("Item \"" + itemName + "\" in town \"" + townName + "\" replenished " + replenishAmount.toString() + " stock and now has " + newCurStock.toString() + " stock.");
+											plugin.saveYamls();
+										}
+									}, 0L, (replenishTime));
+									
+								} else {
+									player.sendMessage(plugin.pre + "Quantity \"" + quantity.toString() + "\" invalid. Please specify a value below the maximum stock level (NO. HERE).");
+								}
+							} else {
+								player.sendMessage(plugin.pre + "Item \"" + itemName + "\" does not exist.");
+							}
+						} else {
+							player.sendMessage(plugin.pre + "Town \"" + townName + "\" does not exist.");
+						}
+						sign.setLine(0, (ChatColor.BLUE + "[Vendor]"));
+					} else {
+						player.sendMessage(plugin.pre + "Invalid number of parameters!");
+					}
+				} else if (sign.getLine(0).equalsIgnoreCase(ChatColor.BLUE + "[Vendor]")) {
+					Player player = event.getPlayer();
+					String [] signText = sign.getLines();
+					String townName = signText[1];
+					String itemName = signText[2].toLowerCase();
+					Integer quantity = Integer.parseInt(signText[3]);
+					double tax = plugin.towns.getInt(townName + ".tax");
+					double preTax = -1;
+					double finalPrice = -1;
+					
+					if (!plugin.towns.getKeys(true).contains(townName + ".items." + itemName)) // if item isn't created
+					{
+						plugin.towns.set(townName + ".items." + itemName + ".curstock", plugin.basePrices.getInt(itemName + ".maxstock"));
+						plugin.saveYamls();
+						preTax = plugin.basePrices.getInt(itemName + ".price") * quantity;
+						finalPrice = preTax + ((preTax / 100) * tax);
+					} else if (plugin.towns.getConfigurationSection(townName + ".items." + itemName).getKeys(false).contains("price")) {
+						preTax = plugin.towns.getInt(townName + ".items." + itemName + ".price") * quantity;
+						finalPrice = preTax + ((preTax / 100) * tax);
+					} else {
+						preTax = plugin.basePrices.getInt(itemName + ".price") * quantity;
+						finalPrice = preTax + ((preTax / 100) * tax);
+					}
+					player.sendMessage(plugin.pre + "Buy " + quantity.toString() + " " + itemName + "(s) from " + townName + " for " + finalPrice + "?");
+				}
 			}
 		} else if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-			if (event.getClickedBlock().getType() == Material.SIGN_POST || event.getClickedBlock().getType() == Material.WALL_SIGN)
+			if (event.getClickedBlock().getState() instanceof Sign)
 			{
+				Sign sign = (Sign) event.getClickedBlock().getState();
 				Player player = event.getPlayer();
-				player.sendMessage("Right clicked on a sign.");
+				// player.sendMessage(plugin.pre + "You right clicked a sign. Well-fucking-done.");
+				String [] signText = sign.getLines();
+				String townName = signText[1];
+				String itemName = signText[2];
+				Integer quantity = Integer.parseInt(signText[3]);
+				Integer curstock = plugin.towns.getInt(townName + ".items." + itemName + ".curstock");
+				if (quantity > curstock)
+				{
+					// Sorry - we're currently out of stock! Our next shipment of X item(s) comes in X days.
+					player.sendMessage(plugin.pre + "Buying quantity larger than current stock.");
+					player.sendMessage(plugin.pre + "I'll sort this out later.");
+				} else {
+					ItemStack items = new ItemStack(Material.getMaterial(itemName.toUpperCase()), quantity);
+					player.getInventory().addItem(items);
+					player.updateInventory();
+					Integer newStock = curstock - quantity;
+					plugin.towns.set(signText[1] + ".items." + signText[2] + ".curstock", newStock);
+					plugin.saveYamls();
+					// player.sendMessage(plugin.pre + "In seriousness, you took " + quantity.toString() + " of the available " + curstock.toString() + " stock.");
+					// player.sendMessage(plugin.pre + "Current stock level is now " + newStock.toString() + ".");
+				}
 			}
 		}
 	}
